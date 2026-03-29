0d792f7899d4be9e201833655125af466f58011e
Kamil Faber <kfaber@agh.edu.pl>
2026-03-29 14:16:41 +0200

    Add Forgetting Measure

--- src/pyclad/metrics/continual/concepts_metric.py
+++ src/pyclad/metrics/continual/concepts_metric.py
@@ -1,23 +1,22 @@
 import abc
 from typing import List
 
-ResultsMatrix = List[List[float]]
+ConceptLevelMatrix = List[List[float]] # ConceptLevelMatrix[learned_concept][evaluated_concept]
 
 
-class ConceptsResultsMatrix(abc.ABC):
+class ConceptLevelMetric(abc.ABC):
+    """Base class for metrics that transform the concept-level metric matrix to summarized single value metric."""
     @abc.abstractmethod
-    def compute(self, metric_matrix: ResultsMatrix) -> float: ...
+    def compute(self, metric_matrix: ConceptLevelMatrix) -> float: ...
 
     @abc.abstractmethod
     def name(self) -> str: ...
 
 
-ConceptLevelMatrix = List[List[float]]
-
-
-class ConceptLevelMetric(abc.ABC):
+class PerLearnedConceptMetric(abc.ABC):
+    """Base class for metrics that transform the concept-level metric matrix a separate value for each learned concept."""
     @abc.abstractmethod
-    def compute(self, metric_matrix: ConceptLevelMatrix) -> float: ...
+    def compute(self, metric_matrix: ConceptLevelMatrix) -> List[float]: ...
 
     @abc.abstractmethod
     def name(self) -> str: ...
